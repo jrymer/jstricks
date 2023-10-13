d9d4ff2e2a3becf42048eabfb3e10b8ee3983471
Josh Rymer <josh.rymer@maxar.com>
2023-10-13 12:36:40 -0700

    more linked list stuffs

--- src/linkedLists/algorithms.ts
+++ src/linkedLists/algorithms.ts
@@ -19,3 +19,47 @@ export const printLinkedListedRecursive = (head?: LLNode): string | null => {
   printLinkedListedRecursive(head.next);
   return head.val as string;
 };
+
+export const valuesIteratively = (head: LLNode) => {
+  if (!head) return;
+
+  let current: LLNode | undefined = head;
+  const values: string[] = [];
+
+  while (current !== undefined) {
+    values.push(current.val as string);
+    current = current.next;
+  }
+
+  return values;
+};
+
+export const valuesRecusrivelyMine = (head: LLNode | undefined): string[] => {
+  if (!head) return [];
+  return [head.val as string, ...valuesRecusrivelyMine(head.next)];
+};
+
+const fillValues = (head: LLNode | undefined, values: string[]) => {
+  if (!head) return [];
+  values.push(head.val as string);
+  fillValues(head.next, values);
+};
+export const valuesRecusrivelyTheirs = (head: LLNode) => {
+  /**
+   * Note that the benefit of splitting the recusrion out into its own function and little section is that in my version, im creating a new array every time, and in this version, we just push values onto the existing values array giving O(n) runtime where as mine is O(n^2)
+   */
+  const values: string[] = [];
+  fillValues(head, values);
+  return values;
+};
+
+export const sumListIteratively = (head: LLNode) => {
+  return 0;
+};
+
+export const sumListRecursivelyMine = (head: LLNode) => {
+  return 0;
+};
+export const sumListRecursivelyTheirs = (head: LLNode) => {
+  return 0;
+};
--- src/linkedLists/linkedLists.test.ts
+++ src/linkedLists/linkedLists.test.ts
@@ -11,10 +11,42 @@ b.next = c;
 c.next = d;
 
 describe("Linked List tests", () => {
-  it("Prints linked list in order iteratively", () => {
-    expect(algos.printLinkedListIterative(a)).toEqual("a,b,c,d");
+  describe("Prints the list in order", () => {
+    it("Iteratively", () => {
+      expect(algos.printLinkedListIterative(a)).toEqual("a,b,c,d");
+    });
+    it("Recursively", () => {
+      expect(algos.printLinkedListIterative(a)).toEqual("a,b,c,d");
+    });
   });
-  it("Prints linked list in order recursively", () => {
-    expect(algos.printLinkedListIterative(a)).toEqual("a,b,c,d");
+  describe("Values in order", () => {
+    it("Iteratively", () => {
+      expect(algos.valuesIteratively(a)).toEqual(["a", "b", "c", "d"]);
+    });
+    it("Recursively my version", () => {
+      expect(algos.valuesRecusrivelyMine(a)).toEqual(["a", "b", "c", "d"]);
+    });
+    it("Recursively their version", () => {
+      expect(algos.valuesRecusrivelyTheirs(a)).toEqual(["a", "b", "c", "d"]);
+    });
+  });
+  describe("Sum list", () => {
+    const a = new LLNode(1);
+    const b = new LLNode(3);
+    const c = new LLNode(-2);
+    const d = new LLNode(7);
+
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    it("Iteratively", () => {
+      expect(algos.valuesIteratively(a)).toEqual(9);
+    });
+    it("Recursively my version", () => {
+      expect(algos.valuesRecusrivelyMine(a)).toEqual(9);
+    });
+    // it("Recursively their version", () => {
+    //   expect(algos.valuesRecusrivelyTheirs(a)).toEqual(["a", "b", "c", "d"]);
+    // });
   });
 });
