d1fc9bf9d4688f0731cb599b06dfb37992041964
Josh Rymer <jrymer1@gmail.com>
2023-10-23 19:00:51 -0700

    array and string problems from the book

---
+++ src/arraysAndStrings/algorithms.ts
@@ -0,0 +1,152 @@
+// pg 90
+
+// Sets dont allow duplicates
+// function containsDuplicate(nums: number[]): boolean {
+//     const set = new Set<number>(nums);
+//     return (set.size < nums.length);
+// };
+export const stringHasUnique = (str: string) => {
+  let unique = true;
+
+  for (let i = 0; i < str.length; i++) {
+    const lastIndex = str.lastIndexOf(str[i]);
+
+    if (i !== lastIndex) {
+      unique = false;
+    }
+  }
+
+  return unique;
+};
+
+export const isPermutation = (s1: string, s2: string) => {
+  if (s2.includes(s1)) {
+    return true;
+  }
+
+  const truthArray = [];
+  const s1Sorted = s1.split("").sort().join("");
+
+  for (let i = 0; i < s2.length; i++) {
+    const upperBound = i + s1.length;
+
+    if (upperBound > s2.length) {
+      i = s2.length - 1;
+    }
+    const ss = s2.substring(i, upperBound).split("").sort().join("");
+
+    truthArray.push(ss === s1Sorted);
+  }
+  return truthArray.includes(true);
+};
+
+export const urlify = (str: string) => {
+  return str.split(" ").join("$");
+};
+
+export const palindromeCheck = (str: string) => {
+  const reversed: string[] = [];
+
+  str
+    .replace(/\s/g, "")
+    .split("")
+    .forEach((letter) => reversed.unshift(letter));
+
+  return reversed.join("") === str.split(" ").join("");
+};
+
+export const oneAway = (s1: string, s2: string) => {
+  if (s2.length >= s1.length + 2) {
+    return false;
+  }
+
+  let miss = 0;
+
+  // pale
+  // ple
+
+  //bale
+  //pale
+
+  //ple
+  //pale
+  const s1Split = s1.split("");
+  const s2Split = s2.split("");
+
+  let j = 0;
+  for (let i = 0; i < s1.length; i++) {
+    if (s1Split[i] !== s2Split[j]) {
+      miss++;
+
+      if (s1Split[i + 1] && s1Split[i + 1] === s2Split[j]) {
+        i++;
+      } else if (s2Split[j + 1] && s2Split[j + 1] === s1Split[i]) {
+        j++;
+      }
+    }
+
+    j++;
+  }
+
+  return miss <= 1;
+};
+
+export const compress = (str: string) => {
+  const counts: string[] = [];
+
+  //aabcccccaaa
+
+  let i = 0;
+  let letterCount = 0;
+  let letter = "";
+
+  while (i <= str.length) {
+    const current = str.split("")[i];
+
+    if (letter === current) {
+      letterCount++;
+    } else {
+      if (i !== 0) {
+        counts.push(`${letter}${letterCount}`);
+      }
+      letter = current;
+      letterCount = 1;
+    }
+
+    i++;
+  }
+
+  return counts.join("").length > str.length ? str : counts.join("");
+};
+
+export const mergeSorted = (
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+) => {
+  if (m === 0) {
+    return nums2;
+  } else if ((n = 0)) {
+    return nums1;
+  }
+
+  //[1,2,3,0,0,0]
+  //[2,5,6]
+  let counter = 0;
+  while (nums2.length) {
+    const num1 = nums1[counter];
+    const num2 = nums2[0];
+
+    if (num2 < num1 || num1 === 0) {
+      if (counter > 0) {
+        nums1 = [...nums1.slice(0, counter), num2, ...nums1.slice(counter)];
+        nums1.pop();
+        nums2.shift();
+      }
+    }
+    counter++;
+  }
+
+  return nums1;
+};
---
+++ src/arraysAndStrings/arrayAndStrings.test.ts
@@ -0,0 +1,39 @@
+import * as algos from "./algorithms";
+
+describe("Array and string problems", () => {
+  it("Checks if string has all unique characters", () => {
+    expect(algos.stringHasUnique("44")).toBeFalsy();
+    expect(algos.stringHasUnique("117")).toBeFalsy();
+    expect(algos.stringHasUnique("132")).toBeTruthy();
+  });
+  it("Checks if string is permuatation of itself", () => {
+    expect(algos.isPermutation("ab", "eidbaooo")).toBeTruthy();
+    expect(algos.isPermutation("ab", "eidboaoo")).toBeFalsy();
+    expect(algos.isPermutation("abc", "bbbca")).toBeTruthy();
+  });
+  it("Replaces spaces in string with $", () => {
+    expect(algos.urlify("Mr John Smith")).toEqual("Mr$John$Smith");
+  });
+  it("Checks if palindrome", () => {
+    expect(algos.palindromeCheck("taco cat")).toBeTruthy();
+  });
+  it("Checks if one edit away", () => {
+    expect(algos.oneAway("pale", "ple")).toBeTruthy();
+    expect(algos.oneAway("pales", "pale")).toBeTruthy();
+    expect(algos.oneAway("pale", "bale")).toBeTruthy();
+    expect(algos.oneAway("pale", "bake")).toBeFalsy();
+  });
+  it("Compresses a string", () => {
+    expect(algos.compress("aabcccccaaa")).toEqual("a2b1c5a3");
+    expect(algos.compress("abcd")).toEqual("abcd");
+  });
+  it("Merges a sorted array", () => {
+    // https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+    expect(algos.mergeSorted([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)).toEqual([
+      1, 2, 2, 3, 5, 6,
+    ]);
+    expect(algos.mergeSorted([1], 1, [], 0)).toEqual([1]);
+    expect(algos.mergeSorted([0], 0, [1], 1)).toEqual([1]);
+  });
+});
